e3f7e6a417957b91c2b2b79046aaca485f14ff5e
Doug Miller <doug.miller@aimia.com>
2017-02-23 09:07:03 +0000

    multiple changes

--- app/app.js
+++ app/app.js
@@ -25,6 +25,7 @@
 		'btobhistogram',
 		'donut',
 		'donuts',
+		'datamaps',
 	]);
 
 })();
--- app/modules/home/homeCtrl.js
+++ app/modules/home/homeCtrl.js
@@ -27,6 +27,27 @@
 		vm.title = "Hello, angmat2!";
 		vm.version = "1.0.0";
 		vm.listFeatures = homeService.getFeaturesList();
+		vm.displayToast = function(){alert("Hello")};
+		vm.mapObject = {
+			scope: 'world',
+			options: {
+				width: 1110,
+				legendHeight: 60 // optionally set the padding for the legend
+			},
+			geographyConfig: {
+				highlighBorderColor: '#EAA9A8',
+				highlighBorderWidth: 2
+			},
+			fills: {
+				'HIGH': '#CC4731',
+				'MEDIUM': '#306596',
+				'LOW': '#667FAF',
+				'defaultFill': '#DDDDDD'
+			},
+			data: {
+
+			},
+		}
 
 	}
 
